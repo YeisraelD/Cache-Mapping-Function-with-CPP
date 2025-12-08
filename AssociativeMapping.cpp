c8dd2901251569f73e361a1628f512a9eedbea53
YeisraelD <Yeisraeld@gmail.com>
2025-12-08 15:13:09 +0300

    evict main cache line and place it in the victim cache

--- AssociativeMapping.cpp
+++ AssociativeMapping.cpp
@@ -29,11 +29,66 @@ int main() {
 
     for (int block : memoryAccesses) {
         int idx = findBlock(mainCache, block);
-        if (idx != -1 ) { // hit in main cache
+        if (idx != -1 ) { 
+            // hit in main cache
             hits++;
             cout << "Block " << block << ": HIT in Main Cache\n";
-        } else{ // miss in main cache
-            
+        } else{ 
+            // miss in main cache
+            int victimIdx = findBlock(victimCache, block);
+
+            if (victimIdx != -1) {
+                // hit in victim cache , so we can swap it now with main cache
+                victimHits++;
+                cout << "Block "<< block << ": HIT in Victim Cache (Swap with Main Cache)";
+
+                int mainReplace =-1;
+                for (int i =0 ; i < mainCache.size(); i++){
+                    if (!mainCache[i].valid) {
+                        mainReplace =i;
+                        break;
+                    }
+                }
+                if (mainReplace == -1) mainReplace =0; // evict first line
+
+                swap(mainCache[mainReplace].blockNumber, victimCache[victimIdx].blockNumber);
+                mainCache[mainReplace].valid = true;
+                victimCache[victimIdx].valid = true;
+
+            } else {
+                // miss in both caches , i mean like a normal miss in direct mapping
+                misses++;
+                cout<< "Block: " << block << " : MISS\n";
+
+                // so now new need to evict the line from main cache
+                int mainReplace =-1;
+                for (int i =0 ; i < mainCache.size(); i++){
+                    if (!mainCache[i].valid) {
+                        mainReplace =i;
+                        break;
+                    }
+                }
+                if (mainReplace == -1) mainReplace =0;
+
+                // then we need to move the evicted block to victim cache
+                // here it is , the main add advantage of associative mapping
+                if (mainCache[mainReplace].valid) {
+                    // we have to find a place in victim cache
+                    int vReplace = -1;
+                    for (int i =0; i < victimCache.size(); i++){
+                        if (!victimCache[i].valid){
+                            vReplace = i ;
+                            break;
+                        }
+                    }
+                    if (vReplace == -1) vReplace =0; // evict first line of victim cache
+                    victimCache[vReplace].blockNumber = mainCache[mainReplace].blockNumber;
+                    victimCache[vReplace].valid = true;
+                }
+
+                
+            }
+
 
         }
     }
